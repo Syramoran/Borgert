99170f5cd3093dcd65436df645b42f8ef31ad282
Syramoran <syramoranicr@gmail.com>
2025-10-11 22:10:13 -0300

    Botón whatsapp

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './componentes/navbar/navbar';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { Footer } from './componentes/footer/footer';
+import { Whatsapp } from './componentes/whatsapp/whatsapp';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent, FontAwesomeModule, Footer],
+  imports: [RouterOutlet, NavbarComponent, FontAwesomeModule, Footer, Whatsapp],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
--- src/app/componentes/whatsapp/whatsapp.ts
+++ src/app/componentes/whatsapp/whatsapp.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { 
+  faWhatsapp 
+} from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'app-whatsapp',
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './whatsapp.html',
   styleUrl: './whatsapp.css'
 })
 export class Whatsapp {
-
+  public faWhatsapp = faWhatsapp;
 }
